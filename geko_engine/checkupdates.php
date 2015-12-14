da8a6bc120ea23c529ac8c85ebdf67894962b7e9
Elías Rodríguez Martín <erm@igeko.es>
2015-12-14 02:23:14 +0000

    Updated geko_engine

--- geko_engine/checkupdates.php
+++ geko_engine/checkupdates.php
@@ -7,21 +7,24 @@ function geko_check_update( $transient ) {
         return $transient;
     }
 
-    $theme_data = wp_get_theme();
+    $theme_data = wp_get_theme(wp_get_theme()->template);
     $theme_slug = $theme_data->get_template();
     
-    $result = json_decode(wp_remote_get("http://linuxgnublog.org/update.json")['body'], true);
+    $remote_version = '0.0.0';
+    $style_css = wp_remote_get("https://raw.githubusercontent.com/IgekoSC/".$theme_slug."/master/style.css")['body'];
+    if ( preg_match( '/^[ \t\/*#@]*' . preg_quote( 'Version', '/' ) . ':(.*)$/mi', $style_css, $match ) && $match[1] )
+        $remote_version = _cleanup_header_comment( $match[1] );
     
-    if (version_compare($theme_data->version, $result['new_version'], '<')) {
+    if (version_compare($theme_data->version, $remote_version, '<')) {
         $transient->response[$theme_slug] = array(
             'theme'       => $theme_slug,
-            'new_version' => $result['new_version'],
-            'url'         => $result['url'],
-            'package'     => $result['package'],
+            'new_version' => $remote_version,
+            'url'         => 'https://github.com/IgekoSC/'.$theme_slug,
+            'package'     => 'https://github.com/IgekoSC/'.$theme_slug.'/archive/master.zip',
         );
     }
         
     return $transient;
 }
 
-// add_filter( 'pre_set_site_transient_update_themes', 'geko_check_update' );
+add_filter( 'pre_set_site_transient_update_themes', 'geko_check_update' );
