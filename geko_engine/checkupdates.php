3151950115dcde77cf3e247dd386a17fa1cb7055
Elías Rodríguez Martín <erm@igeko.es>
2016-08-27 00:27:06 +0100

    Moved geko engine

--- geko_engine/checkupdates.php
+++ geko_engine/checkupdates.php
@@ -11,21 +11,21 @@ function geko_check_update( $transient ) {
     $theme_slug = $theme_data->get_template();
     //Delete '-master' from the end of slug
     $theme_uri_slug = preg_replace('/-master$/', '', $theme_slug);
-    
+
     $remote_version = '0.0.0';
-    $style_css = wp_remote_get("https://raw.githubusercontent.com/IgekoSC/".$theme_uri_slug."/master/style.css")['body'];
+    $style_css = wp_remote_get("https://raw.githubusercontent.com/erm2587/".$theme_uri_slug."/master/style.css")['body'];
     if ( preg_match( '/^[ \t\/*#@]*' . preg_quote( 'Version', '/' ) . ':(.*)$/mi', $style_css, $match ) && $match[1] )
         $remote_version = _cleanup_header_comment( $match[1] );
-    
+
     if (version_compare($theme_data->version, $remote_version, '<')) {
         $transient->response[$theme_slug] = array(
             'theme'       => $theme_slug,
             'new_version' => $remote_version,
-            'url'         => 'https://github.com/IgekoSC/'.$theme_uri_slug,
-            'package'     => 'https://github.com/IgekoSC/'.$theme_uri_slug.'/archive/master.zip',
+            'url'         => 'https://github.com/erm2587/'.$theme_uri_slug,
+            'package'     => 'https://github.com/erm2587/'.$theme_uri_slug.'/archive/master.zip',
         );
     }
-        
+
     return $transient;
 }
 
